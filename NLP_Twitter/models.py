4fc497e730362dd5341d0efa6e8f24ff19fbd553
OMKAR REDDY <omkarreddy.s@gmail.com>
2022-01-29 15:33:52 +0530

    useful_commit

--- NLP_Twitter/models.py
+++ NLP_Twitter/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    topic_text = models.CharField(max_length=15)
+    topic_text = models.CharField(max_length=50)
 
     def __str__(self):
         return self.topic_text
